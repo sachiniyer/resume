a5e265a22c2d3787052d6ebdfd04678c3aef35af
Sachin Iyer <sachinjiyer@gmail.com>
2022-07-10 01:49:34 -0700

    changing linux env to ip monitor

---
+++ .auctex-auto/resume.el
@@ -0,0 +1,39 @@
+(TeX-add-style-hook
+ "resume"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "letterpaper" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fullpage" "empty") ("color" "usenames" "dvipsnames") ("hyperref" "colorlinks=true" "linkcolor=black" "anchorcolor=black" "citecolor=black" "filecolor=black" "menucolor=black" "runcolor=black" "urlcolor=black")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "latexsym"
+    "fullpage"
+    "titlesec"
+    "marvosym"
+    "color"
+    "verbatim"
+    "enumitem"
+    "hyperref"
+    "fancyhdr")
+   (TeX-add-symbols
+    '("resumeSubItem" 2)
+    '("resumeSubheading" 4)
+    '("certItem" 2)
+    '("resumeItem" 2)
+    "resumeSubHeadingListStart"
+    "resumeSubHeadingListEnd"
+    "resumeItemListStart"
+    "resumeItemListEnd"))
+ :latex)
+
